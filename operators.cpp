56d36a07731922f891dd72b42787e110f5bbe498
aditya-123450 <shivam980101@gmail.com>
2024-04-20 00:21:37 +0530

    operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 4;
+
+    cout<<"sum: "<<num1+num2<<endl;
+     cout<<"Difference: "<<num1-num2<<endl;
+     cout<<"product: "<<num1*num2<<endl;
+    cout<<"division: "<<num1/num2<<endl;
+        cout<<"Reminder:  "<<num1%num2<<endl;
+
+
+
+//increment or decrement operator
+    int a=5;
+    cout<<a++<<endl;
+    //a=6
+    cout<<a--<<endl;
+    cout<<a<<endl;
+
+    return 0;
+}
