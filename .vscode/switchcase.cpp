6175ef2456a1a260df21ce9309ebdc1dd7a22317
aditya-123450 <shivam980101@gmail.com>
2024-04-24 18:30:09 +0530

    switch case program

---
+++ .vscode/switchcase.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int day;
+    cin>>day;
+
+    switch(day){
+
+    case 1:
+    cout<<"Monday"<<endl;
+    break;
+
+    case 2:
+    cout<<"tuesday"<<endl;
+    break;
+
+    case 3:
+    cout<<"wednesday"<<endl;
+    break;
+
+    case 4:
+    cout<<"thusday"<<endl;
+    break;
+
+    case 5:
+    cout<<"friday"<<endl;
+    break;
+    
+    default:
+    cout<<"default case"<<endl;
+    
+    }
+
+    
+
+    return 0;
+}
