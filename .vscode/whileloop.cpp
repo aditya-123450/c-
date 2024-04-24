d548102284b7e5f6df0664f8683361eed1c97fd5
aditya-123450 <shivam980101@gmail.com>
2024-04-24 19:16:46 +0530

    while loop program

---
+++ .vscode/whileloop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int n;
+  cin>>n;
+
+  int sum=0;
+
+  int i=1;//loop variable
+  while(i<=n){
+    sum+=i;
+    i++;//updating loop variable
+  }    
+
+  cout<<sum<<endl;
+    return 0;
+}
