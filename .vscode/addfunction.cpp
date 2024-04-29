73c033ffa0d8185046d8ef73063362c045988779
aditya-123450 <shivam980101@gmail.com>
2024-04-29 19:26:07 +0530

    standard lib fun

--- .vscode/addfunction.cpp
+++ .vscode/addfunction.cpp
@@ -5,24 +5,10 @@ int add(int num1, int num2){
     int sum=num1+num2;
     return sum;
 }
-
-int add(int num1, int num2,int num3){
-    int sum=num1+num2+num3;
-    return sum;
-}
-
-float add(float num1, float num2){
-    float sum=num1+num2;
-    return sum;
-}
 int main(){
 
 int a=5;
-int b=4;
-
-float c=3.4;
-float d=4.6;
-
+int b=4; 
 cout<<add(a,b)<<endl;
 
     return 0;
