bd753f93e49bf61b3d09b514258e2d981bd786f1
aditya-123450 <shivam980101@gmail.com>
2024-06-07 11:42:55 +0530

    sort by parity

--- sort01array.cpp
+++ sort01array.cpp
@@ -6,11 +6,14 @@ void sortzeroesandones(vector<int>&v){
 
     int zeroes_count = 0;
     
+    //counting zeroes
     for(int ele:v){
         if(ele==0){
             zeroes_count++;
         }
     }
+
+     
     for(int i=0;i<v.size();i++){
         if(i<zeroes_count){
             v[i]=0;
